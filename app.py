d86e4b1fa5eca7fb56b4b073937c8d082ddd8230
Diego Sú Gómez <a01620476@itesm.mx>
2023-09-16 14:13:13 -0600

    Added Model & HTML Design

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template('index.html')
 
 # Ruta para procesar el archivo
-@app.route('/procesar', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def procesar_archivo():
     # Obtener los valores ingresados por el usuario desde el formulario
     back_x = float(request.form['back_x'])
@@ -19,23 +19,33 @@ def procesar_archivo():
     thigh_y = float(request.form['thigh_y'])
     thigh_z = float(request.form['thigh_z'])
 
-    # Luego, puedes usar estos datos para realizar la predicción con tu modelo entrenado
-    datos = [[back_x, back_y, back_z, thigh_x, thigh_y, thigh_z]]  # Asegúrate de que estos datos estén en el formato correcto
+    # Guardar los datos del formulario
+    datos = [[back_x, back_y, back_z, thigh_x, thigh_y, thigh_z]]
+    print(datos)
 
     # Cargar el modelo entrenado de Random Forest Classifier
-    modelo = joblib.load('modelo_random_forest.pkl')
+    modelo = joblib.load('modelo_random_forest.joblib')
 
     # Realizar la predicción utilizando el modelo
     prediccion = modelo.predict(datos)
+    
+    #Mostrar el tipo de actividad dependiendo de la predicción
+    if prediccion == 1:
+        actividad = "Walking / Caminando"
+    elif prediccion == 3:
+        actividad = "Shuffling / Arrastrar los pies"
+    elif prediccion == 4: 
+        actividad = "Stairs (Ascending) / Subir escaleras"
+    elif prediccion == 5:
+        actividad = "Stairs (Descending) / Bajar escaleras"
+    elif prediccion == 6:
+        actividad = "Standing / Estar parado"
+    elif prediccion == 7:
+        actividad = "Sitting / Estar sentado"
+    else:
+        actividad = "Lying / Estar acostado"
 
-    # En lugar de cargar el modelo aquí, debes cargarlo previamente antes de utilizar esta función.
-
-    # La variable 'prediccion' ahora contiene el resultado de tu modelo
-
-    # Puedes devolver este resultado a tu plantilla HTML para mostrarlo
-    return render_template('index.html', resultado=prediccion)
-
-
+    return render_template('index.html', resultado=actividad)
 
 if __name__ == '__main__':
     app.run(debug=True)
